5c9ff8ab15b69482b5e954d7ef1ee64ee4bd40a7
jkillz2020 <kilgore.josh@gmail.com>
2017-07-22 16:12:44 -0500

    add react components up to first state ex

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish){
+    //update our state
+    const fishes = {...this.state.fishes};
+    //add new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    //set state
+    this.setState({ fishes: fishes });
+  }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+}
+
+export default App;
